fb9083062616b3cb90344333dbdd5e549edba8f1
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-09-16 01:06:07 +0600

    correction mobile and tab view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+    },
     extend: {},
+    container: {
+      center:true,
+      padding: {
+        DEFAULT:'1rem',
+        sm:'1.5rem',
+      }
+    }
   },
   plugins: [],
 }
