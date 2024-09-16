26b66045d594c069a1a8c26f0bf6de33b9055f34
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-09-16 20:16:00 +0600

    correcting styleCss

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,27 @@ module.exports = {
       md:"768px",
       lg:"1024px",
     },
-    extend: {},
+    extend: {
+      colors: {
+        firstColor:'#FFEB00',
+        bodyColor: '#FDFAD9',
+        whiteColor: '#fff',
+        secondColor: '#1230AE',
+        thirdColor:'#B8001F',
+        fourthColor:'#FEF9D9'
+      }
+    },
     container: {
       center:true,
       padding: {
         DEFAULT:'1rem',
         sm:'1.5rem',
       }
+    },
+    fontFamily: {
+       playfairFont : ["Playfair Display", 'sans-serif'],
+       acmeFont : ["Acme", 'sans-serif'],
+       suseFont : ["SUSE", 'sans-serif']
     }
   },
   plugins: [],
